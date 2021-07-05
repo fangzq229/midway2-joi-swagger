a5707c4fa8cda8bcd762e360dffdc98c22492ddc
fangzq <m18811126599@gmail.com>
2021-07-05 19:37:43 +0800

    modified:   lib/mid-validate.ts

--- lib/mid-validate.ts
+++ lib/mid-validate.ts
@@ -1,4 +1,4 @@
-import * as joi from 'joi';
+import * as joi from "joi";
 
 /*
  * joi参数校验处理
@@ -7,19 +7,22 @@ const validate = (schemaList: Array<{ ctxkey: string; schemas: any }>): any => {
   return async (ctx: any, next: () => Promise<any>) => {
     try {
       for (const p of schemaList) {
-        // query 参数 number 类型格式化
-        const param = ctx[p.ctxkey];
+        const param = p.ctxkey.includes("body")
+          ? ctx.request.body
+          : ctx[p.ctxkey];
         joi.assert(param, p.schemas);
+        // query 参数 number 类型格式化
         const joiArr = p.schemas?.$_terms?.keys || [];
         for (const k in param) {
           const p1 = joiArr.find((i: any) => i.key === k);
-          if (p1.schema?.type === 'number') {
+          if (p1.schema?.type === "number") {
             ctx[p.ctxkey][k] = Number(ctx[p.ctxkey][k]);
           }
         }
       }
     } catch (error) {
-      return ctx.throw(422, JSON.stringify(error.message));
+      const e = error.details ? error.details[0]?.message : "Parameter error";
+      return ctx.throw(422, e);
     }
     await next();
   };
